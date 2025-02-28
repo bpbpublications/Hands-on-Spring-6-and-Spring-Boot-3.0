88f795ab791bc089d108bfb66ea8bdc63bf659b0
BPB Online <41231825+bpbpublications@users.noreply.github.com>
2025-02-28 20:44:40 +0530

    By BPB Publications

--- Chapter 09/2-bookclub-jms/src/main/java/com/bpb/hssb/ch9/bookclub/repository/ReservationRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bpb.hssb.ch9.bookclub.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bpb.hssb.ch9.bookclub.domain.ReservationRequest;
-
-public interface ReservationRequestRepository extends CrudRepository<ReservationRequest, Integer> {
-
-}
--- Chapter 09/2-bookclub-jms/src/main/java/com/bpb/hssb/ch9/bookclub/service/ReservationService.java
+++
@@ -1,9 +0,0 @@
-package com.bpb.hssb.ch9.bookclub.service;
-
-import com.bpb.hssb.ch9.bookclub.domain.ReservationRequest;
-
-public interface ReservationService {
-
-    public ReservationRequest createReservationRequest(ReservationRequest reservationRequest);
-
-}
